dbf68cb50c97ef081ad9dea8a705a0c47425faa9
Moiz Asad <moizasad86@gmail.com>
2022-05-24 18:20:45 +0500

    First Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = ()=>{
+  return (
+    <>
+    <h1>Hello, ACM Workshop</h1>
+    <h1>Hello, ACM Workshop</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import App from './App'
+
+const Element = document.getElementById("root");
+
+const Root = ReactDOM.createRoot(Element);
+
+Root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+
+
+
+
+
+
+// const root = ReactDOM.createRoot();
+
+
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
